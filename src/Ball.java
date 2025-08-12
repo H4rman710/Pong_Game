5d5d8219dd138e231c73b3235e6f5d315017397e
H4rman <harmansalzbrunn710@gmail.com>
2025-08-12 15:13:35 +0100

    added instances

--- src/Ball.java
+++ src/Ball.java
@@ -3,8 +3,28 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class Ball {
+public class Ball extends Rectangle{
+
+    Random random;
+    int yVelocity;
+    int xVolicity;
+
+
     Ball(){
 
     }
+
+    public void setXDirection(int randomXDirection){
+
+    }
+    public void setYDirection(int randomYDirection){
+
+    }
+    public void move(){
+
+    }
+    public void draw(Graphics g) {
+
+    }
+
 }
