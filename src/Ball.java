e2cb10cd686d3f3eb95ca0a2a72194e4704532b8
H4rman <harmansalzbrunn710@gmail.com>
2025-08-13 15:20:35 +0100

    Fixed the issues with the paddles and background

--- src/Ball.java
+++ src/Ball.java
@@ -9,13 +9,23 @@ public class Ball extends Rectangle{
     int yVelocity;
     int xVolicity;
 
+    Ball(int x,int y,int width,int height){
+        super(x,y,width,height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if(randomXDirection==0)
+            randomXDirection--;
+        setXDirection(randomXDirection);
 
-    Ball(){
+        int randomYDirection = random.nextInt(2);
+        if(randomYDirection==0)
+            randomYDirection--;
+        setYDirection(randomYDirection);
 
     }
 
     public void setXDirection(int randomXDirection){
-
+        xVolicity
     }
     public void setYDirection(int randomYDirection){
 
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -33,13 +33,14 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
     public void newBall(){
-
+        //random = new Random();
+        ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),(GAME_HEIGHT/2)-(BALL_DIAMETER/2),BALL_DIAMETER,BALL_DIAMETER);
     }
     public void newPaddles(){
         paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
-        paddle2 = new Paddle(GAME_WIDTH-PADDLE_HEIGHT,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
     }
-    public void newPaint(Graphics g){
+    public void paint(Graphics g){
         image = createImage(getWidth(),getHeight());
         graphics = image.getGraphics();
         draw(graphics);
@@ -51,10 +52,19 @@ public class GamePanel extends JPanel implements Runnable{
         paddle2.draw(g);
     }
     public void move(){
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
     public void checkCollision(){
-
+        if(paddle1.y<=0)
+            paddle1.y=0;
+        if(paddle1.y>=(GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle1.y=GAME_HEIGHT-PADDLE_HEIGHT;
+        if(paddle2.y<=0)
+            paddle2.y=0;
+        if(paddle2.y>=(GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle2.y=GAME_HEIGHT-PADDLE_HEIGHT;
     }
     public void run(){
 //game loop
@@ -76,10 +86,12 @@ public class GamePanel extends JPanel implements Runnable{
     }
     public class AL extends KeyAdapter{
         public void keyPressed(KeyEvent e){
-
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
         }
         public void keyReleased(KeyEvent e){
-
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
         }
     }
 }
